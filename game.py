48e0ec756485a80c1302c4bec543d6e33ca56fc5
DESKTOP-RG3IC9D\alvar <alvaro82vr@gmail.com>
2021-09-05 18:40:07 +0200

    con @property

--- game.py
+++ game.py
@@ -11,11 +11,49 @@ class Movil(): # Esta clase me va a permitir que defina Raqueta como un Movil. L
         self.h = h  # alto Movil
         self.color = color #
 
+    @property # Decorador = pasa una función a atributo (sería como un getter)
+    def izquierda(self): # Como es un método lleva (self)
+        return self.x
+
+    @izquierda.setter # Decorador (sería como definir un setter). Sintaxis = nombre de la propiedad.setter (La propiedad, en este caso "izquierda" tiene que estar definida anteriormente como propiedad. Lo hizo en el @property superior)
+    def izquierda(self, valor):
+        self.x = valor
+
+    @property
+    def derecha(self):
+        return self.x + self.w
+
+    @derecha.setter
+    def derecha(self, valor):
+        self.x = valor - self.w
+
+    @property
+    def arriba(self):
+        return self.y
+
+    @arriba.setter
+    def arriba(self, valor):
+        self.y = valor
+
+    @property
+    def abajo(self):
+        return self.y + self.h
+
+    @abajo.setter
+    def abajo(self, valor):
+        self.y = valor - self.h
+
+
+
+
     def actualizate(self):
         pass
 
     def dibujate(self, lienzo): # Método para que se pinte en pantalla, en una surface "lienzo". Voy a tener que informar la surface donde quiero que lo dibuje (lienzo)
         pg.draw.rect(lienzo, self.color, pg.Rect(self.x, self.y, self.w, self.h)) # Lo pinta en la surface, con mi color, mi x, mi y, mi w, mi h
+
+    def procesa_eventos(self, lista_eventos=[]): # Método de Movil para procesar los movimientos de las raquetas. Si le pongo la lista_eventos vacia, puedo darsela ya informada o hacer un procesa eventos
+        pass
 
 
 class Raqueta(Movil): # Se añade el Movil
@@ -26,9 +64,22 @@ class Raqueta(Movil): # Se añade el Movil
         #self.w = w  # ancho Raqueta- Al meter el Movil, este atributo sobra, ya lo hace Movil
         #self.h = h  # alto Raqueta- Al meter el Movil, este atributo sobra, ya lo hace Movil
         #self.color = color #- Al meter el Movil, este atributo sobra, ya lo hace Movil
+        self.tecla_arriba = pg.K_UP # Atributo para definir las teclas para los eventos
+        self.tecla_abajo = pg.K_DOWN
 
+    def procesa_eventos(self, lista_eventos=[]): # Metodo pra procesar eventos
+        if pg.key.get_pressed()[self.tecla_arriba]: # Es unmétodo de pygame, devuelve True si la tecla está pulsada o False si no lo está
+            self.y -= 5
 
+        if self.arriba <= 0: # Límites pra que la raqueta no se pierda por la parte sup/inf de la pantalla
+            self.arriba = 0
+        if self.abajo >= TAMANNO[1]:
+            self.abajo = TAMANNO[1]
 
+        if pg.key.get_pressed()[self.tecla_abajo]: # Es un método de pygame, devuelve True si la tecla está pulsada o False si no lo está
+            self.y += 5
+       
+        
 class Bola(Movil): # Ahora HEREDA de Movil
     def __init__(self, x, y, color=(255, 255, 255)): # Constructor, con sus parámetros a definir
         super().__init__(x, y, 20, 20, color) # Esto, super(), es lo mismo que: "Movil.__init__(self, x, y, 20, 20, color)". Es otra sintaxis para HEREDAR
@@ -37,31 +88,31 @@ class Bola(Movil): # Ahora HEREDA de Movil
         #self.w = w  # ancho Bola - Al meter el Movil, este atributo sobra, ya lo hace Movil
         #self.h = h  # alto Bola - Al meter el Movil, este atributo sobra, ya lo hace Movil
         #self.color = color # - Al meter el Movil, este atributo sobra, ya lo hace Movil
-        self.derecha = True # Atributo para el cambio de dirección de la bola (eje x)
-        self.arriba = True # Atributo para el cambio de dirección de la bola (eje y)
+        self.swDerecha = True # Atributo para el cambio de dirección de la bola (eje x) - Le cambia el nombre de self.derecha a swDerecha ????
+        self.swArriba = True # Atributo para el cambio de dirección de la bola (eje y) - Le cambia el nombre de self.ariiba a swArriba ????
 
     def actualizate(self): # Método de la clase Bola (para que se mueva sola)
-        if self.derecha: # Si self.derecha está a True:
+        if self.swDerecha: # Si self.derecha está a True:
             self.x += 5 # Velocidad hacia la derecha
         else:
             self.x -= 5 # Velocidad hacia la izquierda
 
-        if self.x + self.w >= TAMANNO[0]: # Condición cuando la bola toca el límite 800
-            self.derecha = False # Cambia de dirección
+        if self.izquierda >= TAMANNO[0]: # Condición cuando la bola toca el límite 800 . Antes del los @property : "if self.x + self.w >= TAMANNO[0]:
+            self.swDerecha = False # Cambia de dirección
 
-        if self.x <= 0: # Condición cuando la bola toca el límite 0
-            self.derecha = True # Cambia de dirección
+        if self.derecha <= 0: # Condición cuando la bola toca el límite 0 - # Antes de los property :"if self.x"
+            self.swDerecha = True # Cambia de dirección
 
-        if self.arriba: # Si self.arriba está a True:
+        if self.swArriba: # Si self.arriba está a True:
             self.y -= 5 # Velocidad hacia arriba
         else:
             self.y += 5 # Velocidad hacia abajo
 
-        if self.y + self.h >= TAMANNO[1]: # Condición cuando la bola toca el límite 600
-            self.arriba = True # Velocidad hacia arriba
+        if self.abajo >= TAMANNO[1]: # Condición cuando la bola toca el límite 600 . Antes de los @ property: "if self.y + self.h >= TAMANNO[1]:""
+            self.swArriba = True # Velocidad hacia arriba
 
-        if self.y <= 0: 
-            self.arriba = False # Velocidad hacia abajo
+        if self.arriba <= 0:  # Antes de los property :"if self.y"
+            self.swArriba = False # Velocidad hacia abajo
 
 
 class Game():
@@ -72,6 +123,8 @@ class Game():
         self.todos = [] # Crea una lista vacia para posteriormente añadir ahi todos los objetos que nos interese meter, para iterarlos con un bucle y ahorrar código = CREAR LISTA VACIA
         
         self.player1 = Raqueta(10, (TAMANNO[1] -120) // 2) # Intancia Raqueta 1, con sus parametros definidos (posición x, y, tamaño ancho, alto (definidos en la su clase) y color por defecto) = CREAR PLAYER1
+        self.player1.tecla_arriba = pg.K_w # Define teclas para player1
+        self.player1.tecla_abajo = pg.K_s # Define teclas para player 1
         self.player2 = Raqueta(TAMANNO[0] -30, (TAMANNO[1] - 120) // 2) # Intancia Raqueta 2, con sus parametros definidos (posición x, y, tamaño ancho, alto (definidos en su clase) y color por defecto) = CREAR PLAYER2
 
         self.todos.append(self.player1) # Añade a la lista de self.todos la raqueta 1
@@ -96,11 +149,30 @@ class Game():
             for evento in eventos:
                 if evento.type == pg.QUIT:
                     game_over = True # Si uno de los eventos de event.get() es de tipo cerrar "X", que se cierre pygame, poniendo game_over a True
+                '''
+                if evento.type == pg.KEYDOWN: # Si el evento es del tipo presionar tecla
+                    if evento.key == pg.K_UP: # evento.key, solo será informado si el evento.type es de tipo KEYDOWN. K_UP es propio de pygame (flecha arriba)
+                        self.player2.y -= 5 # La raqueta 2 se mueve 5 hacia arriba. Le informo quien se mueve (self.player2), en que eje se mueve (.y) y cuanto se mueve (-5)
+                    if evento.key == pg.K_DOWN:
+                        self.player2.y += 5
+
+                    if evento.key == pg.K_w:
+                        self.player1.y -= 5
+                    if evento.key == pg.K_s:
+                        self.player1.y += 5
+                '''
+                '''
+            # Control d eventos para movimiento de las raquetas
+            self.player1.procesa_eventos() # Llamada al método de procesar eventos de Raqueta. Hay que diferenciar las teclas de movimiento. Creamos self.tecla_arriba, abajo en clase Game, al instaciar raqueta
+            self.player2.procesa_eventos() # Llamada al método de procesar eventos de Raqueta. Hay que diferenciar las teclas de movimiento. Creamos self.tecla_arriba, etc en la clase raqueta
+                '''
+            
 
             #self.bola.actualizate() # Llama al método actualizate de la clase Bola, es donde está definido el movimiento de bola
             #for i in range(1): # Llama al método actualizate de la clase Bola, pero para todo el bucle de bolas creadas con el bucle de crear bolas
                 #self.bolas[i].actualizate()
             for movil in self.todos: # Iteración para actualizar los personajes. Cuando le toque a bola, ejecuta el def actualizate() de la clase Bola. Cuando le toque a la raqueta (no tiene método actualizate, cogerá el de Movil)
+                movil.procesa_eventos() # Aprovecha la iteración para procesar los eventos de raqueta (sustituye a los 2 parrafos comentados de arriba)
                 movil.actualizate()
 
 
